cb616a82450d0c071b74a733eb672a76a373b6d5
Cash Costello <cash.costello@jhuapl.edu>
2022-12-08 15:03:48 -0500

    adds projects, batches, and permissions

--- turkle_client/bin.py
+++ turkle_client/bin.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-from .client import Groups, Users
+from .client import Batches, Groups, Permissions, Projects, Users
 
 users_choices = ['list', 'create', 'retrieve', 'update']
 users_help = """list      List all users as jsonl
@@ -15,6 +15,26 @@ groups_help = """list      List all groups as jsonl
 create    Create new groups
 retrieve  Retrieve a group selected by name or integer identifier
 addusers  Add users to an existing group by passing their ids
+"""
+
+projects_choices = ['list', 'create', 'retrieve', 'update']
+projects_help = """list      List all projects as jsonl
+create    Create new projects
+retrieve  Retrieve a project based on integer identifier
+update    Update projects
+"""
+
+batches_choices = ['list', 'create', 'retrieve', 'update']
+batches_help = """list      List all batches as jsonl
+create    Create new batches
+retrieve  Retrieve a batch based on integer identifier
+update    Update batches
+"""
+
+perm_choices = ['retrieve', 'add', 'replace']
+perm_help = """retrieve  Retrieve permissions for a project or batch
+add       Add users or groups to a project's or batch's permissions
+replace   Replace a project's or batch's permissions
 """
 
 
@@ -34,7 +54,7 @@ class Cmd:
         users_parser.add_argument('subcommand', choices=users_choices, help=users_help)
         users_parser.add_argument('--id', help='User id (integer) - for retrieve')
         users_parser.add_argument('--username', help='Username - for retrieve')
-        users_parser.add_argument('--file', help='Json file - required for create and update')
+        users_parser.add_argument('--file', help='Jsonl file - required for create and update')
 
         groups_parser = subparsers.add_parser(
             'groups',
@@ -45,16 +65,38 @@ class Cmd:
         groups_parser.add_argument('subcommand', choices=groups_choices, help=groups_help)
         groups_parser.add_argument('--id', help='User id - required for retrieve')
         groups_parser.add_argument('--name', help='Group name - for retrieve')
-        groups_parser.add_argument('--file', help='Json file - required for create or addusers')
+        groups_parser.add_argument('--file', help='Jsonl file - required for create or addusers')
 
-        projects_parser = subparsers.add_parser('projects',
-                                                help='List, create, or update projects.')
+        projects_parser = subparsers.add_parser(
+            'projects',
+            help='List, create, or update projects.',
+            formatter_class=argparse.RawTextHelpFormatter
+        )
         self.update_title(projects_parser)
-        projects_parser.add_argument('subcommand')
+        projects_parser.add_argument('subcommand', choices=projects_choices, help=projects_help)
+        projects_parser.add_argument('--id', help='Project id - required for retrieve')
+        projects_parser.add_argument('--file', help='Jsonl file - required for create or update')
 
-        batches_parser = subparsers.add_parser('batches', help='List, create, or update batches.')
+        batches_parser = subparsers.add_parser(
+            'batches',
+            help='List, create, or update batches.',
+            formatter_class=argparse.RawTextHelpFormatter
+        )
         self.update_title(batches_parser)
-        batches_parser.add_argument('subcommand')
+        batches_parser.add_argument('subcommand', choices=batches_choices, help=batches_help)
+        batches_parser.add_argument('--id', help='Batch id - required for retrieve')
+        batches_parser.add_argument('--file', help='Jsonl file - required for create or update')
+
+        perm_parser = subparsers.add_parser(
+            'permissions',
+            help='List, add, or replace permissions.',
+            formatter_class=argparse.RawTextHelpFormatter
+        )
+        self.update_title(perm_parser)
+        perm_parser.add_argument('subcommand', choices=perm_choices, help=perm_help)
+        perm_parser.add_argument('--pid', help='Project id')
+        perm_parser.add_argument('--bid', help='Batch id')
+        perm_parser.add_argument('--file', help='Jsonl file - required for add or replace')
 
     @staticmethod
     def update_title(parser, title='Subcommand'):
--- turkle_client/client.py
+++ turkle_client/client.py
@@ -53,6 +53,15 @@ class Client:
         except requests.exceptions.ConnectionError:
             raise ValueError(f"Unable to connect to {self.base_url}")
 
+    def put(self, url, data, *args, **kwargs):
+        try:
+            response = requests.put(url, *args, **kwargs, json=data, headers=self.headers)
+            if response.status_code >= 400:
+                self.handle_errors(response)
+            return response
+        except requests.exceptions.ConnectionError:
+            raise ValueError(f"Unable to connect to {self.base_url}")
+
     def handle_errors(self, response):
         data = response.json()
         if data:
@@ -78,13 +87,13 @@ class Users(Client):
         elif username:
             url = self.Urls.username.format(base=self.base_url, username=username)
         else:
-            raise ValueError(f"--id or --username must be set for 'users retrieve'")
+            raise ValueError("--id or --username must be set for 'users retrieve'")
         response = self.get(url)
         return response.text
 
     def create(self, file, **kwargs):
         if not file:
-            raise ValueError(f"--file must be set for users create")
+            raise ValueError("--file must be set for users create")
         url = self.Urls.list.format(base=self.base_url)
         with open(file, 'r') as fh:
             count = 0
@@ -95,7 +104,7 @@ class Users(Client):
 
     def update(self, file, **kwargs):
         if not file:
-            raise ValueError(f"--file must be set for users update")
+            raise ValueError("--file must be set for users update")
         with open(file, 'r') as fh:
             count = 0
             for line in fh:
@@ -130,7 +139,7 @@ class Groups(Client):
 
     def create(self, file, **kwargs):
         if not file:
-            raise ValueError(f"--file must be set for groups create")
+            raise ValueError("--file must be set for groups create")
         url = self.Urls.list.format(base=self.base_url)
         with open(file, 'r') as fh:
             count = 0
@@ -141,11 +150,142 @@ class Groups(Client):
 
     def addusers(self, id, file, **kwargs):
         if not id:
-            raise ValueError(f"--id must be set for groups addusers")
+            raise ValueError("--id must be set for groups addusers")
         if not file:
-            raise ValueError(f"--file must be set for groups addusers")
+            raise ValueError("--file must be set for groups addusers")
         url = self.Urls.addusers.format(base=self.base_url, id=id)
         with open(file, 'r') as fh:
             data = json.load(fh)
             self.post(url, data)
         return f"{plural(len(data['users']), 'user', 'users')} add to the group"
+
+
+class Projects(Client):
+    class Urls:
+        list = "{base}/api/projects/"
+        detail = "{base}/api/projects/{id}/"
+
+    def list(self, **kwargs):
+        url = self.Urls.list.format(base=self.base_url)
+        return self.walk(url)
+
+    def retrieve(self, id=None, **kwargs):
+        if not id:
+            raise ValueError("--id must be set for 'projects retrieve'")
+        url = self.Urls.detail.format(base=self.base_url, id=id)
+        response = self.get(url)
+        return response.text
+
+    def create(self, file, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for projects create")
+        url = self.Urls.list.format(base=self.base_url)
+        with open(file, 'r') as fh:
+            count = 0
+            for line in fh:
+                data = json.loads(line.strip())
+                if 'html_template' not in data:
+                    with open(data['filename'], 'r') as csv_fh:
+                        data['html_template'] = csv_fh.read()
+                        data['filename'] = os.path.basename(data['filename'])
+                self.post(url, data)
+                count += 1
+        return f"{plural(count, 'project', 'projects')} created"
+
+    def update(self, file, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for projects update")
+        with open(file, 'r') as fh:
+            count = 0
+            for line in fh:
+                data = json.loads(line.strip())
+                if 'html_template' not in data and 'filename' in data:
+                    with open(data['filename'], 'r') as csv_fh:
+                        data['html_template'] = csv_fh.read()
+                        data['filename'] = os.path.basename(data['filename'])
+                url = self.Urls.detail.format(base=self.base_url, id=data['id'])
+                self.patch(url, data)
+                count += 1
+        return f"{plural(count, 'project', 'projects')} updated"
+
+
+class Batches(Client):
+    class Urls:
+        list = "{base}/api/batches/"
+        detail = "{base}/api/batches/{id}/"
+
+    def list(self, **kwargs):
+        url = self.Urls.list.format(base=self.base_url)
+        return self.walk(url)
+
+    def retrieve(self, id=None, **kwargs):
+        if not id:
+            raise ValueError(f"--id must be set for 'batches retrieve'")
+        url = self.Urls.detail.format(base=self.base_url, id=id)
+        response = self.get(url)
+        return response.text
+
+    def create(self, file, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for batches create")
+        url = self.Urls.list.format(base=self.base_url)
+        with open(file, 'r') as fh:
+            count = 0
+            for line in fh:
+                data = json.loads(line.strip())
+                with open(data['filename'], 'r') as csv_fh:
+                    data['csv_text'] = csv_fh.read()
+                    data['filename'] = os.path.basename(data['filename'])
+                self.post(url, data)
+                count += 1
+        return f"{plural(count, 'batch', 'batches')} created"
+
+    def update(self, file, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for batches update")
+        with open(file, 'r') as fh:
+            count = 0
+            for line in fh:
+                data = json.loads(line.strip())
+                url = self.Urls.detail.format(base=self.base_url, id=data['id'])
+                self.patch(url, data)
+                count += 1
+        return f"{plural(count, 'batch', 'batches')} updated"
+
+
+class Permissions(Client):
+    class Urls:
+        projects = "{base}/api/projects/{id}/permissions/"
+        batches = "{base}/api/batches/{id}/permissions/"
+
+    def get_url(self, pid=None, bid=None):
+        if pid:
+            url = self.Urls.projects.format(base=self.base_url, id=pid)
+        elif bid:
+            url = self.Urls.batches.format(base=self.base_url, id=bid)
+        else:
+            raise ValueError("--pid or --bid is required")
+        return url
+
+    def retrieve(self, pid=None, bid=None, **kwargs):
+        url = self.get_url(pid, bid)
+        response = self.get(url)
+        return response.text
+
+    def add(self, pid=None, bid=None, file=None, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for permissions add")
+        url = self.get_url(pid, bid)
+        with open(file, 'r') as fh:
+            data = json.load(fh)
+            self.post(url, data)
+        return "Permissions updated"
+
+    def replace(self, pid=None, bid=None, file=None, **kwargs):
+        if not file:
+            raise ValueError("--file must be set for permissions replace")
+        url = self.get_url(pid, bid)
+        with open(file, 'r') as fh:
+            data = json.load(fh)
+            self.put(url, data)
+        return "Permissions updated"
